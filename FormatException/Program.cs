37021de705f1d96f4c6f1a95f391d95b809b8810
baish <baish@BAISHNABI>
2024-10-17 13:26:00 +0530

    Add project files.

---
+++ FormatException/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("your age");
+        int age;
+        string line = Console.ReadLine();
+        try
+        {
+            age = int.Parse(line);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("{0} is not integer", line);
+        }
+    }
+
+}
+
